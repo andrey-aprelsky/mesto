3e7af4f1fd1ccd691cf212837b16d3cf1065fe5c
Andrey Mosiagin <andrey-aprelsky@yandex.ru>
2022-04-08 20:02:37 +0300

    feature/complete5

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,34 @@
-const editButton = document.querySelector('.profile__edit-button');
-const overlay = document.querySelector('.overlay');
-const closeOverlay = document.querySelector('.form-container__close-button');
+const editButton = document.querySelector(".profile__edit-button");
+const overlay = document.querySelector(".popup");
+const closeOverlay = document.querySelector(".popup__close-button");
+const form = document.querySelector(".popup__form");
+
+const nameInput = document.querySelector(".popup__input_name");
+const title = document.querySelector(".profile__title");
+
+const descriptionInput = document.querySelector(".popup__input_description");
+const description = document.querySelector(".profile__subtitle");
 
 function openOverlay() {
-  overlay.classList.toggle ('overlay_is-active')
+  overlay.classList.add("popup_is-active");
+  nameInput.value = title.textContent;
+  descriptionInput.value = description.textContent;
 }
 
-editButton.addEventListener('click', openOverlay );
-
-closeOverlay.addEventListener('click', openOverlay);
-
-function clickOpenOverlay(event) {
-  if (event.target === event.currentTarget) {
-    
-    openOverlay()
-  }
-
+function close() {
+  overlay.classList.remove("popup_is-active");
 }
 
-overlay.addEventListener('click', clickOpenOverlay);
-
-
-const form = document.querySelector('.form');
-
-const nameInput = document.querySelector('.form__input-name');
-const title = document.querySelector('.profile__title')
-
-const descriptionInput = document.querySelector('.form__input-description'); 
-const description = document.querySelector('.profile__subtitle');
-
-function onSubmit(e) {
-  e.preventDefault()
+function formSubmitHandler(e) {
+  e.preventDefault();
   title.textContent = nameInput.value;
   description.textContent = descriptionInput.value;
 
-  openOverlay()
+  close()
 }
 
-form.addEventListener('submit', onSubmit);
+editButton.addEventListener("click", openOverlay);
+
+closeOverlay.addEventListener("click",close);
+
+form.addEventListener("submit", formSubmitHandler);
